4f31b568d446ec8e41ad8f1c955460c20b67d5e9
alsongard <alsongard@gmail.com>
2025-01-01 19:52:28 +0300

    java datatypes and variables

---
+++ datatypes_variables.java
@@ -0,0 +1,32 @@
+package tutorial;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helo World");
+		
+//		 int  == integer value
+		int value_one = 10;
+		System.out.println(value_one);
+		
+//		double == decimal value
+		double value_two = 21.25;
+		System.out.println(value_two);
+		
+//		boolean true | false
+		boolean answer = true;
+		System.out.println(answer);
+		
+//		char datatype
+		char c = 't';
+		System.out.println(c);
+		
+//		string datatype
+		String greet = "Welcome to Java FreeCodeCamp tutorial";
+		System.out.println(greet);
+		
+	}
+
+}
+
