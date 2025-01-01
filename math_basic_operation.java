f4474b5733d671322d1e668e8f3217aa188a99e3
alsongard <alsongard@gmail.com>
2025-01-01 23:31:53 +0300

    mathematic basic operations done

---
+++ math_basic_operation.java
@@ -0,0 +1,27 @@
+package tutorial;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 30;
+		int y = 40;
+		int result = x + y;
+		System.out.println(result);
+		
+
+		
+		int new_calc = 10 + 2 * 3 - 4 / 2;
+		System.out.println(new_calc);
+		
+		int value_one = 100;
+		int value_two = 11;
+		double value_result = value_one / (double)value_two;
+		double integer_value = value_one / value_two;
+		System.out.println(value_result);
+		System.out.println(integer_value);
+	}
+
+}
+
