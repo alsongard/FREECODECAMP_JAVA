796727fadf0a493e45e4ef45cf5dbb50e49265da
alsongard <alsongard@gmail.com>
2025-01-05 23:34:08 +0300

    using if else if & else condition

---
+++ if_conditions.java
@@ -0,0 +1,44 @@
+package tutorial;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter what aspired you: ");
+		String user_input = sc.next();
+		
+		// Conditions and Loops
+		if (user_input.equals("future"))
+		{
+			System.out.println("Welcome to the future");
+		}
+		else
+		{
+			System.out.println("Need to increase speed.");
+		}
+		
+		// if(condition){action} else if(condition){action} else{action}
+		
+		System.out.println("Enter any number: ");
+		int user_value = sc.nextInt();
+		if (user_value <= 10) {
+			System.out.println("The number you entered is greater than 10 : " + user_value);
+		}
+		else if (user_value <= 20)
+		{
+			System.out.println("The number you entered is less than 20 : " + user_value);
+		}
+		else
+		{
+			System.out.println("The number you entered is greater than 21 : " + user_value);
+		}
+		
+				
+	}
+
+}
+
