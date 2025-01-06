a195ea4ee098c777d54eb6bbf8e01e363b936768
alsongard <alsongard@gmail.com>
2025-01-06 22:15:11 +0300

    ArrayList added

---
+++ arrayList.java
@@ -0,0 +1,58 @@
+/*
+ * ArrayList
+ * Syntax: 
+ * ArrayList<dataType> varialbeName = new ArrayList<dataType>();
+ * .add(a) : add value(a) to arrayList
+ * .get(a) : return value(a) at given index
+ * .set(a, b) : at the given index(a) set to value(b)
+ * */
+
+package tutorial;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.HashSet;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// arrayList
+		
+		ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+		
+		myNumbers.add(10);
+		System.out.println(myNumbers.get(0)); // the arrayListVariableName.get(index) returns the value at the given index
+		
+		myNumbers.add(15);
+		System.out.println("myNumber array before changes : "+ myNumbers);
+		
+		// change the value for the given index
+		myNumbers.set(1, 5);
+		
+		System.out.println(myNumbers);
+		
+		for (int a = 0; a < 20; a++)
+		{
+			a = a * 2;
+			myNumbers.add(a);
+		}
+		
+		//.size(()
+		System.out.println(myNumbers.size());  // returns the lenght fo the list
+		System.out.println(myNumbers);
+		
+		//.subList()
+		System.out.println(myNumbers.subList(0,  5));
+		
+		
+		
+		
+	}
+
+
+}
+
