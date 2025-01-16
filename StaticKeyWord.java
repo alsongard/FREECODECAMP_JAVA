24f8908d717837e4d33b721cd607dec43c6e641c
alsongard <alsongard@gmail.com>
2025-01-16 23:07:40 +0100

    getting to know static variables

---
+++ StaticKeyWord.java
@@ -0,0 +1,29 @@
+/*
+ * static variable is incremented at each instance of the class(object)
+ */
+
+public class StaticKeyWord {
+
+    public static int staticCounter = 0;
+    public int instanceCounter = 0;
+
+
+    // constructor function which is created at each new instance of the class that is 
+    // when creating an object
+    public StaticKeyWord()
+    {
+        staticCounter++; 
+        System.out.println("Value fo staticCounter is : " + staticCounter);
+        instanceCounter++;
+
+    }
+    public static void main(String[] args)
+    {
+        StaticKeyWord object1 = new StaticKeyWord(); // creating an object the constructor method is called
+        StaticKeyWord object2 = new StaticKeyWord(); // creating an object the constructor method is called
+
+        System.out.println("StaticCounter is : " + staticCounter); // gets incremented again once called
+        System.out.println("instanceCounter object1 is : " + object1.instanceCounter);
+        System.out.println("instanceCounter object2 is : " + object2.instanceCounter);
+    }
+}
